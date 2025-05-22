30f2d435cebd4a8e2eaab558a5afd4f1bd5c0f9a
bhanuka2 <bhanukashashiindra.com>
2025-05-22 11:11:46 +0530

    Second Commit

--- src/main/java/edu/icet/ecom/controller/HomePageController.java
+++ src/main/java/edu/icet/ecom/controller/HomePageController.java
@@ -1,11 +1,14 @@
 package edu.icet.ecom.controller;
 
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+
+import java.awt.*;
 
 public class HomePageController {
 
@@ -15,15 +18,25 @@ public class HomePageController {
     @FXML
     private ListView txtListview;
 
+
+
+
     @FXML
     void btnAddOnAction(ActionEvent event) {
 
-        String Task=txtAdd.getText();
-        CheckBox checkBox=new CheckBox(Task);
+        String task = txtAdd.getText();
+        CheckBox checkBox = new CheckBox(task);
 
         txtListview.getItems().add(checkBox);
-        new Alert(Alert.AlertType.CONFIRMATION ,"Added Successfully").show();
 
+        if (checkBox.isSelected()) {
+            System.out.println("SELECTED");
+                 }
     }
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
 
+        int selectedID = txtListview.getSelectionModel().getSelectedIndex();
+        txtListview.getItems().remove(selectedID);
+    }
 }
